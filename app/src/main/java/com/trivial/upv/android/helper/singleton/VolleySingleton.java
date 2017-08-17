026218d5ad6c8686ddf5e2d29397fa2866c2ee1a
jvg637upv <jvg637upv@gmail.com>
2017-08-17 12:34:38 +0200

    17/06/2017 Se cambia la cola de peticiones

--- app/src/main/java/com/trivial/upv/android/helper/singleton/VolleySingleton.java
+++ app/src/main/java/com/trivial/upv/android/helper/singleton/VolleySingleton.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.support.v4.util.LruCache;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.StringRequest;
@@ -13,44 +14,56 @@ import com.android.volley.toolbox.Volley;
  * Created by jvg63 on 22/01/2017.
  */
 public class VolleySingleton {
-    private static VolleySingleton volleySingleton = null;
+    private static VolleySingleton mInstance = null;
 
-    public static VolleySingleton getInstance(Context context) {
-        if (volleySingleton == null)
-            synchronized (VolleySingleton.class) {
-                if (volleySingleton == null)
-                    volleySingleton = new VolleySingleton(context);
-            }
-        return volleySingleton;
+    public static synchronized VolleySingleton getInstance(Context context) {
+        if (mInstance == null)
+            mInstance = new VolleySingleton(context);
+
+        return mInstance;
     }
 
-    public static RequestQueue getColaPeticiones() {
-        return requestQueue;
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext());
+        }
+        return mRequestQueue;
     }
 
-    public static ImageLoader getLectorImagenes() {
-        return imgReader;
-    }
-
-    private static StringRequest stringRequest;
-
-    private static RequestQueue requestQueue;
-    private static ImageLoader imgReader;
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+    private static Context mContext;
 
     private VolleySingleton(Context context) {
 
-        requestQueue = Volley.newRequestQueue(context);
+        mRequestQueue = Volley.newRequestQueue(context);
+        mContext = context.getApplicationContext();
 
-        imgReader = new ImageLoader(requestQueue, new ImageLoader.ImageCache() {
-            private final LruCache<String, Bitmap> cache = new LruCache<String, Bitmap>(20);
+        mRequestQueue = getRequestQueue();
 
-            public void putBitmap(String url, Bitmap bitmap) {
-                cache.put(url, bitmap);
-            }
+        mImageLoader = new ImageLoader(mRequestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<String, Bitmap>(20);
 
-            public Bitmap getBitmap(String url) {
-                return cache.get(url);
-            }
-        });
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+    }
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader() {
+        return mImageLoader;
     }
 }
